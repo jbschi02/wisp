87928076afa5f0ad6055cc3aa79ef2ddeb520799
Braxton Eveslage <beeves01@louisville.edu>
2019-04-20 13:56:33 -0400

    Remove Destructible interface from ImageRegister.sol contract

--- contracts/FeedRegister.sol
+++ contracts/FeedRegister.sol
@@ -1,8 +1,6 @@
 pragma solidity ^0.4.24;
 
-import "openzeppelin-solidity/contracts/lifecycle/Destructible.sol";
-
-contract ImageRegister is Destructible {
+contract ImageRegister {
 
     struct Feed {
         string ipfsHash;
@@ -12,31 +10,19 @@ contract ImageRegister is Destructible {
 
     mapping (address => Feed) public userAddressToFeedMapping;
 
-    bool private stopped = false;
-
     event FeedUpdatedOrAdded(
         address _userAddress, 
         string _ipfsHash, 
-        string _userAlias
+        string _userAlias,
         uint256 _dateLastModified
     );
 
-    event LogEmergencyStop(
-        address indexed _owner, 
-        bool _stop
-    );
-
-    modifier stopInEmergency { 
-        require(!stopped); 
-        _;
-    }
-
     function() public {}
 
-    function updateFeed(
+    function addOrUpdateFeed(
         string _ipfsHash, 
         string _userAlias
-    ) public stopInEmergency returns (bool _success) {
+    ){
             
         require(bytes(_ipfsHash).length == 46);
         require(bytes(_userAlias).length > 0 && bytes(_userAlias).length <= 256);
@@ -45,29 +31,27 @@ contract ImageRegister is Destructible {
         Feed memory feed = Feed(
             _ipfsHash,
             _userAlias,
-            uploadedOn
+            dateLastModified
         );
 
         userAddressToFeedMapping[msg.sender] = feed;
 
-        emit FeedUpdated(
+        emit FeedUpdatedOrAdded(
             msg.sender,
             _ipfsHash,
             _userAlias,
             dateLastModified
         );
-
-        _success = true;
     }
 
     function getFeed(address _userAddress) 
-        public stopInEmergency view returns (
+    returns (
         string _ipfsHash, 
         string _userAlias,
         uint256 _dateLastModified
     ) {
 
-        require(_owner != 0x0);
+        require(_userAddress != 0x0);
 
         Feed storage feed = userAddressToFeedMapping[_userAddress];
         
@@ -76,16 +60,5 @@ contract ImageRegister is Destructible {
             feed.userAlias,
             feed.dateLastModified
         );
-    }
-
-    /**
-    * @notice Pause the contract. 
-    * It stops execution if certain conditions are met and can be useful 
-    * when new errors are discovered. 
-    * @param _stop Switch the circuit breaker on or off
-    */
-    function emergencyStop(bool _stop) public onlyOwner {
-        stopped = _stop;
-        emit LogEmergencyStop(owner, _stop);
     }
 }
