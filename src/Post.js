9297bbad75bcea4913f84531776bf9675a821317
John <jbschi02@louisville.edu>
2019-04-22 23:40:34 -0400

    implemented delete post feature

---
+++ src/Post.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+const moment = require('moment');
+
+class Post extends Component {
+	constructor(props) {
+		super(props);
+
+		this.handleDelete = this.handleDelete.bind(this); 
+		this.handleReply = this.handleReply.bind(this);
+	}
+
+	handleReply() {
+		this.props.replyButtonClicked();
+	}
+
+	handleDelete() {
+		this.props.deletePost(this.props.id);
+	}
+
+	render() {
+		var time = moment(this.props.timestamp).format("h:mm MM/DD/YYYY");
+		return (
+      <div>
+        <div>
+          <div className="wispPost">
+            <header> 
+            <div className="wispPostOwner">
+            {this.props.alias}
+            </div>
+            </header>
+            <div className="wispPostBody">
+              <div className="wispPostContent">
+              {this.props.content}
+              </div>
+              <div>
+                <div>
+                  <button onClick={this.handleReply} className="userActionButton">
+                  Reply
+                  </button>
+                  <button onClick={this.handleDelete} className="userActionButton">
+                  Delete
+                  </button>
+                </div>
+                <div className="wispPostDate">
+                {time}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+		)
+	}
+}
+export default Post;
